0f932f7e7d0c81daf6a6b40411cbc10672520550
nmskm240 <UTC.AM240@gmail.com>
2024-04-11 23:52:05 +0000

    グラフの日付を実データと合わせる

--- src/Infra/src/TradeHistoryGraphPrinter.cs
+++ src/Infra/src/TradeHistoryGraphPrinter.cs
@@ -7,44 +7,62 @@ namespace Infra;
 
 public class TradeHistoryGraphPrinter
 {
-    private PlotModel Chart { get; init; }
-    private DateTimeAxis DateTimeAxis { get; init; }
-    private CandleStickSeries CandleStickSeries { get; init; }
+    const string GRAPTH_TITLE = "TradeHistory";
+    const string FILE_NAME = $"{GRAPTH_TITLE}.pdf";
+
+    private PlotModel? Chart { get; set; }
+    private DateTimeAxis? DateTimeAxis { get; set; }
+    private CandleStickSeries? CandleStickSeries { get; set; }
 
     public TradeHistoryGraphPrinter(Exchange exchange)
     {
-        DateTimeAxis = new DateTimeAxis();
-        CandleStickSeries = new CandleStickSeries();
-        Chart = new PlotModel();
-        Chart.Axes.Add(DateTimeAxis);
-        Chart.Series.Add(CandleStickSeries);
-
         exchange.OnFetchedCandle.Subscribe(
-            PlotOHLCV,
+            OnNext,
             Output
         );
+    }
+
+    private void OnNext(Candle candle)
+    {
+        if (Chart == null)
+        {
+            Console.WriteLine(candle.Date);
+            CandleStickSeries = new CandleStickSeries();
+            DateTimeAxis = new DateTimeAxis
+            {
+                Minimum = DateTimeAxis.ToDouble(candle.Date),
+                Maximum = DateTimeAxis.ToDouble(DateTime.Now),
+            };
+            Chart = new PlotModel()
+            {
+                Title = GRAPTH_TITLE,
+            };
+            Chart.Axes.Add(DateTimeAxis);
+            Chart.Series.Add(CandleStickSeries);
+        }
+        PlotOHLCV(candle);
     }
 
     private void PlotOHLCV(Candle candle)
     {
         var item = new HighLowItem
         {
-            X = CandleStickSeries.Items.Count,
+            X = DateTimeAxis.ToDouble(candle.Date),
             Open = (double)candle.Open,
             High = (double)candle.High,
             Low = (double)candle.Low,
             Close = (double)candle.Close,
         };
         CandleStickSeries.Items.Add(item);
-        DateTimeAxis.DataMinimum 
     }
 
     private void Output()
     {
-        Chart.InvalidatePlot(true);
-        using var stream = File.Create("hoge.pdf");
+        Chart!.InvalidatePlot(true);
+        using var stream = File.Create(FILE_NAME);
         var exporter = new PdfExporter()
         {
+            // MEMO: ひとまず適当な長さに設定
             Width = 3000,
             Height = 600,
         };
