f30ee3f3b4c9646680321a98bb293ca49d71a086
nmskm240 <UTC.AM240@gmail.com>
2024-07-15 12:54:35 +0000

    feat bitfinex、bitmex、bitbankが2020年以前のデータを取得できるため対応

--- src/Domain/src/Symbol.cs
+++ src/Domain/src/Symbol.cs
@@ -3,14 +3,20 @@ namespace BotTrade.Domain;
 //         複数取引所での稼働を考えるなら修正が必要
 public enum Symbol
 {
+    [EnumString("BTCUSD")]
+    Future_BTCUSD,
     [EnumString("BTCUSDT")]
     Future_BTCUSDT,
     [EnumString("ETHUSDT")]
     Future_ETHUSDT,
+    [EnumString("BTC/USD")]
+    Spot_BTCUSD,
     [EnumString("BTC/USDT")]
     Spot_BTCUSDT,
     [EnumString("ETH/USDT")]
     Spot_ETHUSDT,
     [EnumString("ETH/BTC")]
     Spot_ETHBTC,
+    [EnumString("BTC/JPY")]
+    Spot_BTCJPY,
 }
