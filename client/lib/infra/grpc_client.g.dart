b1a763e83fb011b1061072ff74785135e75fa853
nmskm240 <utc.am240@gmail.com>
2024-12-31 10:09:36 +0000

    fix grpcクライアントがランタイム中に破棄されないように修正

--- client/lib/infra/grpc_client.g.dart
+++ client/lib/infra/grpc_client.g.dart
@@ -6,11 +6,11 @@ part of 'grpc_client.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$grpcClientHash() => r'fbfcfb28f0968a39ce9230f5f8e7b28d6c0de855';
+String _$grpcClientHash() => r'cc6591cb9c204e9033e2894d9c3a83bce27fa6c4';
 
 /// See also [grpcClient].
 @ProviderFor(grpcClient)
-final grpcClientProvider = AutoDisposeProvider<_GrpcClient>.internal(
+final grpcClientProvider = Provider<_GrpcClient>.internal(
   grpcClient,
   name: r'grpcClientProvider',
   debugGetCreateSourceHash:
@@ -21,6 +21,6 @@ final grpcClientProvider = AutoDisposeProvider<_GrpcClient>.internal(
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef GrpcClientRef = AutoDisposeProviderRef<_GrpcClient>;
+typedef GrpcClientRef = ProviderRef<_GrpcClient>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
